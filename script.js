c177f113b13606bc018b99170afdc521a0084f5a
CarlosTjoeNij <46446937+CarlosTjoeNij@users.noreply.github.com>
2024-12-16 15:17:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,12 @@
 const apiKey = "8155b35e4cef4b79bff7bbfa39470eac";
 
 // Initialize the map with default coordinates (before getting the user's location)
-const map = L.map('map').setView([0, 0], 2); // Start with a world view
+//const map = L.map('map').setView([0, 0], 2); // Start with a world view
+
+const map = new maplibregl.Map({
+    container: 'my-map',
+    style: 'https://maps.geoapify.com/v1/styles/osm-bright-smooth/style.json?apiKey=YOUR_API_KEY',
+});
 
 // Add Geoapify Tile Layer
 L.tileLayer(`https://maps.geoapify.com/v1/tile/osm-bright/{z}/{x}/{y}.png?apiKey=${apiKey}`, {
